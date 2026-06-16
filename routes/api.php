82468418a8b6db6b497eea28f9bc06d6516a986d
gabs <gab.covalski@gmail.com>
2026-06-16 20:56:00 -0300

    api sanctum auth+jetstream+livewire

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/npc', function (Request $request) {
+    return response()->json([
+        'message' => 'Hello World'
+        ]);
 })->middleware('auth:sanctum');
